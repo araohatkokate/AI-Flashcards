e28752c18109e21fd1977ee008fb7a021158d3f9
araohatkokate <araohatk@gmail.com>
2024-08-19 14:40:04 -0500

    finished api request to openAI

---
+++ app/api/generate/route.js
@@ -0,0 +1,44 @@
+import {NextResponse} from 'next/server'
+import OpenAI from 'openai'
+
+const systemPrompt = `You are an AI flashcard generator. Your task is to create concise, informative flashcards based on the provided content. Each flashcard should include a question or prompt on one side and a clear, accurate answer or explanation on the other. Focus on key concepts, definitions, and important details. Keep the language simple and easy to understand, and ensure that each flashcard helps reinforce the user's understanding of the topic.
+
+1. Create clear and concise questions for the front of the flashcard.
+2. Provide accurate and informative answers for the back of the flashcard.
+3. Ensure that each flashcard focuses on a single concept of piece of information.
+4. Use simple language to make the flashcards accesible to a wide range of learners.
+5. Include a variety of questions types, such as definitions, example, comparisons, and applications.
+6. Avoid overly complex or ambiguous phrasing in both questions and answers.
+7. When appropraite, use mnemonics or memory aids to help reinforce the information.
+8. Tailor the difficulty level of the flashcarsd to the user's specified preferences.
+9. If given a body of text, extract the most important and relevant information for the flashcards.
+10. Aim to create a balanced set of flashcards that covers the topic comprehensively.
+
+Return in the following JSON format
+{
+    "flashcards":[
+        {
+            "front": str,
+            "back": str
+        }
+    ]
+}
+`
+
+export async function POST(req){
+    const openai = OpenAI()
+    const data = await req.text()
+
+    const completion = await openai.chat.completion.create({
+        messages: [
+            {role: 'system', content: systemPrompt},
+            {role: 'user', content: data},
+        ],
+        model: "gpt-4o",
+        reponse_format: {type: 'json_object'},
+    })
+
+    const flashcards = JSON.parse(completion.choices[0].message.content)
+
+    return NextResponse.json(flashcards.flashcards)
+}
